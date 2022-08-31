eb5dab82c07eace8fb3a10274f9088ee80b68338
Andrea Hergert <ahergert24@hotmail.com>
2022-08-30 22:37:19 -0700

    added initial style and fuctionality to button along with first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,94 @@ THEN the game is over
 WHEN the game is over
 THEN I can save my initials and my score */
 
+var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
+var questionContainerElement = document.getElementById("question-container");
+var questionElement = document.getElementById("question");
+var answerButtonsElement = document.getElementById("answer-buttons");
 
+var shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+// Start game
+function startGame() {
+    startButton.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove("hide");
+    setNextQuestion();
+}
+
+// Setting next question will be what happens when next button is pressed
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+            (answerButtonsElement.firstChild)
+    }
+}
+// Sets what happens when answer is selected
+function selectAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "Restart"
+        startButton.classList.remove("hide")
+    }
+}
+
+    function setStatusClass(element, correct) {
+        clearStatusClass(element)
+        if (correct) {
+            element.classList.add("correct")
+        } else {
+            element.classList.add("wrong")
+        }
+    }
+
+    function clearStatusClass(element) {
+        element.classList.remove("correct")
+        element.classList.remove("wrong")
+    }
+
+    var questions = [
+        {
+            question: "Commonly used data types DO NOT include:",
+            answers: [
+                { text: "strings", correct: false },
+                { text: "boolean", correct: false },
+                { text: "alerts", correct: true },
+                { text: "numbers", correct: false },
+            ]
+        }
+    ]
