f0f85a6070b51b00e2c86e23d3661dcfbcd13c90
Andrea Hergert <ahergert24@hotmail.com>
2022-08-31 09:45:08 -0700

    added remaining questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,6 +91,7 @@ function selectAnswer(e) {
     }
 
     var questions = [
+        // Question 1
         {
             question: "Commonly used data types DO NOT include:",
             answers: [
@@ -99,5 +100,45 @@ function selectAnswer(e) {
                 { text: "alerts", correct: true },
                 { text: "numbers", correct: false },
             ]
-        }
+        },
+        // Question 2
+        {
+            question: "The condition in an if/else statement is enclosed within ______.",
+            answers: [
+                { text: "quotes", correct: false },
+                { text: "curly brackets", correct: true },
+                { text: "parentheses", correct: false },
+                { text: "square brackets", correct: false },
+            ]
+        },
+        // Question 3
+        {
+            question: "Arrays in JavaScript can be used to store ______.",
+            answers: [
+                { text: "numbers and strings", correct: false },
+                { text: "other arrays", correct: false },
+                { text: "booleans", correct: false },
+                { text: "all of the above", correct: true },
+            ]
+        },
+        // Question 4
+        {
+            question: "String values must be enclosed within ______ when being assigned to variables.",
+            answers: [
+                { text: "commas", correct: false },
+                { text: "curly brackets", correct: false },
+                { text: "quotes", correct: true },
+                { text: "parentheses", correct: false },
+            ]
+        },
+        // Question 5
+        {
+            question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+            answers: [
+                { text: "JavaScript", correct: false },
+                { text: "terminal / bash", correct: false },
+                { text: "for loops", correct: false },
+                { text: "console.log", correct: true },
+            ]
+        },
     ]
