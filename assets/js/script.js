b6dcc94d07c1c860cb4517ce3d2e8ebc2149a61e
Andrea Hergert <ahergert24@hotmail.com>
2022-09-06 12:56:53 -0700

    changed question format for answers to be an array and for next question to push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,192 +1,49 @@
-/*
-Get score to display
-    each question worth 1 point
+//Variables 
+var scores = document.querySelector("#scores");
+var timer = document.querySelector("#timer");
+var container = document.querySelector("#container");
+var title = document.querySelector("#title");
+var content = document.querySelector("#content");
+var start = document.querySelector("#start");
+var answer = document.querySelector("#answer");
+var questionList = [];
+var optionList = [];
+var currentQues = 0;
+var score = 0;
+var timeLeft = 75;
+var isQuizOngoing = false;
+var leaderboard = [];
+var initials = "";
+var isClearingAnswer = false;
+var clearingAnswerCode = 0;
+var isCorrect = false;
 
-Get timer to take away 15 seconds for wrong answers instead of taking away 15 seconds for all answers
-
-Get timer to stop quiz when time runs out
-
-Need text box for users initials when time expires to submit score with a button and save score to local storage
-
-*/
-
-var startButton = document.getElementById("start-btn");
-var nextButton = document.getElementById("next-btn");
-var questionContainerElement = document.getElementById("question-container");
-var questionElement = document.getElementById("question");
-var answerButtonsElement = document.getElementById("answer-buttons");
-var introText = document.getElementById("intro");
-var scorePoint = document.getElementById("score");
-var timerEl = document.getElementById('countdown');
-var allQuestions, currentQuestionIndex
-var timeLeft = 75
-var total = 0;
-
-startButton.addEventListener("click", startGame);
-nextButton.addEventListener("click", () => {
-    currentQuestionIndex++
-    setNextQuestion()
-})
-
-var questions = [
-    // Question 1
-    {
-        question: "Commonly used data types DO NOT include:",
-        answers: [
-            { text: "strings", correct: false },
-            { text: "boolean", correct: false },
-            { text: "alerts", correct: true },
-            { text: "numbers", correct: false },
-        ]
-    },
-    // Question 2
-    {
-        question: "The condition in an if/else statement is enclosed within ______.",
-        answers: [
-            { text: "quotes", correct: false },
-            { text: "curly brackets", correct: true },
-            { text: "parentheses", correct: false },
-            { text: "square brackets", correct: false },
-        ]
-    },
-    // Question 3
-    {
-        question: "Arrays in JavaScript can be used to store ______.",
-        answers: [
-            { text: "numbers and strings", correct: false },
-            { text: "other arrays", correct: false },
-            { text: "booleans", correct: false },
-            { text: "all of the above", correct: true },
-        ]
-    },
-    // Question 4
-    {
-        question: "String values must be enclosed within ______ when being assigned to variables.",
-        answers: [
-            { text: "commas", correct: false },
-            { text: "curly brackets", correct: false },
-            { text: "quotes", correct: true },
-            { text: "parentheses", correct: false },
-        ]
-    },
-    // Question 5
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: [
-            { text: "JavaScript", correct: false },
-            { text: "terminal / bash", correct: false },
-            { text: "for loops", correct: false },
-            { text: "console.log", correct: true },
-        ]
-    },
-]
-
-// Start game shows first question (0 in array/index)
-function startGame() {
-    introText.classList.add("hide");
-    startButton.classList.add("hide");
-    allQuestions = questions;
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove("hide");
-    setNextQuestion();
-    total = 0;
-    // the score should display at the start as 0
-    scorePoint = total
-
-    // Timer that counts down from 75 starts when start button is pressed
-function countdown() {
-        // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-        var timeInterval = setInterval(function () {
-            // As long as the `timeLeft` is greater than 1
-            if (timeLeft > 1) {
-                // Set the `textContent` of `timerEl` to show the remaining seconds
-                timerEl.textContent = timeLeft + ' seconds remaining';
-                // Decrement `timeLeft` by 1
-                timeLeft--;
-            } else if (timeLeft === 1) {
-                // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-                timerEl.textContent = timeLeft + ' second remaining';
-                timeLeft--;
-            } else {
-                // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-                timerEl.textContent = 'Time is up!';
-                // Use `clearInterval()` to stop the timer
-                clearInterval(timeInterval);
-            }
-        }, 1000);
-    }
-    countdown();
-}
-
-// Setting next question in array (question 2, index 1) will be what happens when next button is pressed
-function setNextQuestion() {
-    resetState()
-    showQuestion(allQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-        var button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("btn")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-            // needs to scorePoint when answer is correct
-            scorePoint = {total}
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
-}
-
-// Hides next button while questions are display and replace text inside button to answers in questions
-function resetState() {
-    nextButton.classList.add("hide")
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-            (answerButtonsElement.firstChild)
+// Question format
+class Question {
+    constructor(question, options, answer) {
+        this.question = question;
+        this.options = options;
+        this.answer = answer;
     }
 }
 
-// Sets what happens when answer is selected
-function selectAnswer(event) {
-    var selectedButton = event.target
-    var correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (allQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove("hide")
-    } else {
-        startButton.innerText = "Restart"
-        startButton.classList.remove("hide");
-        scorePoint = {total}
-    }
-    if (correct){
-        // needs to score point when answer is correct
-        total++
-    }
-    // Takes away time for any answer but need to be if correct answer is false
-    if (selectedButton.value !== questions[currentQuestionIndex].answers) {
-        timeLeft -= 15
-        if (timeLeft < 0) {
-            timeLeft = 0
-        }
-    }
-}
+// Questions with answers; answers put into arrays
+var options1 = ["1. strings", "2. boolean", "3. alerts", "4. numbers"];
+var question1 = new Question("Commonly used data types DO NOT include:", options1, "3. alerts");
+questionList.push(question1);
 
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add("correct");
-    } else {
-        element.classList.add("wrong");
-    }
-}
+var options2 = ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"];
+var question2 = new Question("The condition in an if/else statement is enclosed within ______.", options2, "2. curly brackets");
+questionList.push(question2);
 
-function clearStatusClass(element) {
-    element.classList.remove("correct")
-    element.classList.remove("wrong")
-}
+var options3 = ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"];
+var question3 = new Question("What parameters can be passed into the query selector function?", options3, "4. all of the above");
+questionList.push(question3);
+
+var options4 = ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"];
+var question4 = new Question("String values must be enclosed within ______ when being assigned to variables.", options4, "3. quotes");
+questionList.push(question4);
+
+var options5 = ["1. JavaScript", "2. terminal / bash", "3. for loops", "4. console.log"];
+var question5 = new Question("A very useful tool used during development and debugging for printing content to the debugger is:", options5, "4. console.log");
+questionList.push(question5);
